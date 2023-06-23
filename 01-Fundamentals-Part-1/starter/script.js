3596c99034fcfcd810b3b9741d1ccf42fa1a7260
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-23 15:49:12 +0530

    Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,3 +3,21 @@ let js = "amazing";
 if (js == "amazing") {
   alert("JS is Fun");
 }
+
+console.log(40 + 8 + 23);
+
+let name = "Saksham";
+
+name = "Arpan";
+
+console.log(name);
+const firstname = "Saksham";
+// firstname = "Arpan"; //can't be redefine
+
+//Operators
+
+const first = "Saksham";
+const last = "Raghuvanshi";
+
+console.log(first + " " + last);
+console.log(2 ** 3); //2 power 3
