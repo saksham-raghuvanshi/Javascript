0f742094e17617919a5d3999101628bf1df0cb82
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-23 16:45:47 +0530

    Strings

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -42,8 +42,47 @@ const heightMark = 1.6;
 const massjohn = 57;
 const heightjohn = 1.5;
 
-const BMImark = massMark / (heightMark * heightMark);
+const BMImark = massMark / (heightMark * heightMark); //Grouping operator
 const BMIJohn = massjohn / (heightjohn * heightjohn);
 console.log(BMIJohn);
 console.log(BMImark);
 console.log(BMIJohn > BMImark);
+
+//member access
+
+const person = {};
+person["firstname"] = "Saksham";
+person["lastname"] = "Raghuvanshi";
+console.log(person.firstname);
+console.log(person.lastname);
+
+//> .optional chaining is a deature of javascript that allows you ti access properties of an object or elements of an array without having to check wwhetther the object  or array is null or undefine first.or aa=rray
+//it never throw error is telll null or undefined
+const user = {
+  name: "Saksham",
+  age: 20,
+  address: {
+    city: "pathankot",
+    state: "punjab",
+    zip: 145025,
+  },
+};
+
+console.log(user.address.city);
+console.log(user.address.Roadno); //undefined that is good
+// console.log(user.address.Roadno.houseno); //error not good here ?. optional chaining work
+console.log(user.address.Roadno?.houseno); //undefined Now
+console.log(user?.address?.city); //undefined Now
+//String
+
+const users = [
+  { name: "Saksham", age: 20 },
+  { name: "Yugesh", age: 19 },
+];
+
+console.log(users[0]?.name);
+console.log(users[1]?.name);
+
+name = "Saksham";
+const hello = `I'm ${name}`;
+console.log(hello);
