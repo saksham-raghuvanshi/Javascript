faeaa6731d0716475101f237e61d7c36794bdc7e
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-23 16:09:40 +0530

    Operator complete

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -21,3 +21,29 @@ const last = "Raghuvanshi";
 
 console.log(first + " " + last);
 console.log(2 ** 3); //2 power 3
+
+let x = 10 + 3;
+console.log(x);
+x += 10; //x=x+1
+console.log(x);
+x++; //x=x+1;
+console.log(x);
+x--; //x=x-1;
+console.log(x);
+
+// comparison operator >,<,>=,<=
+const age = 20;
+console.log(age > 21);
+
+let BMI;
+
+const massMark = 67;
+const heightMark = 1.6;
+const massjohn = 57;
+const heightjohn = 1.5;
+
+const BMImark = massMark / (heightMark * heightMark);
+const BMIJohn = massjohn / (heightjohn * heightjohn);
+console.log(BMIJohn);
+console.log(BMImark);
+console.log(BMIJohn > BMImark);
